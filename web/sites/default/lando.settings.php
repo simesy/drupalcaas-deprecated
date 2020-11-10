6c2c2aaeb0b72303b70ce309fe3149d04aa342ce
Simon Hobbs <simon@hobbs.id.au>
2020-11-10 13:15:07 +1100

    Some config changes, settings file structure, and lando baseline.

---
+++ web/sites/default/lando.settings.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Copy this file to settings.local.php to override local stuff.
+ */
+
+$databases['default']['default'] = array (
+  'database' => 'drupal9',
+  'username' => 'drupal9',
+  'password' => 'drupal9',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/lando.services.yml';
+
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$settings['twig_debug'] = TRUE;
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.logging']['error_level'] = 'verbose';
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+$settings['update_free_access'] = FALSE;
+$settings['rebuild_access'] = TRUE;
+$settings['skip_permissions_hardening'] = TRUE;
+
+$config['system.logging']['error_level'] = 'verbose';
+
--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,43 +1,34 @@
 <?php
 /**
  * @file
- * Platform.sh example settings.php file for Drupal 8.
+ * Base settings file.
+ * @see https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
  */
 
-// Default Drupal settings.
-//
-// These are already explained with detailed comments in Drupal's
-// default.settings.php file.
-//
-// See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
 $databases = [];
 $config_directories = [];
+$settings['hash_salt'] = 'Not the same as the hosting platform.';
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 $settings['file_scan_ignore_directories'] = [
   'node_modules',
   'bower_components',
 ];
-
-// The hash_salt should be a unique random value for each application.
-// If left unset, the settings.platformsh.php file will attempt to provide one.
-// You can also provide a specific value here if you prefer and it will be used
-// instead. In most cases it's best to leave this blank on Platform.sh. You
-// can configure a separate hash_salt in your settings.local.php file for
-// local development.
-// $settings['hash_salt'] = 'change_me';
-
-// Set up a config sync directory.
-//
-// This is defined inside the read-only "config" directory, deployed via Git.
 $settings['config_sync_directory'] = '../config/sync';
 
-// Automatic Platform.sh settings.
-if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
-  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+// Include a generic Platform.sh settings file if remote.
+$platformsh = new \Platformsh\ConfigReader\Config();
+if ($platformsh->isValidPlatform()) {
+  include $app_root . '/' . $site_path . '/platformsh.settings.php';
+}
+
+
+// Include a generic Lando file if local.
+if (getenv('LANDO') == 'ON') {
+  include $app_root . '/' . $site_path . '/lando.settings.php';
 }
 
 // Local settings. These come last so that they can override anything.
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
+if (file_exists($app_root . '/' . $site_path . '/local.settings.php')) {
+  include $app_root . '/' . $site_path . '/local.settings.php';
 }
