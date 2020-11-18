05dde502885a2daae19ab60d916cb6193bcf5f5a
tan.nguyen <raucaixanh04@gmail.com>
2020-11-18 14:52:28 +0700

    Add circleci script to pull DB

---
+++ .circleci/prepare-environment-variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+curl -sS https://platform.sh/cli/installer | php
+
+if [ -z "${CIRCLE_PULL_REQUEST}" ]; then
+  PLATFORMSH_ENVIRONMENT=${CIRCLE_BRANCH}
+else
+  PR_NUMBER="$(echo ${CIRCLE_PULL_REQUEST} | grep / | cut -d/ -f7-)"
+  PLATFORMSH_ENVIRONMENT="pr-${PR_NUMBER}"
+fi
+echo "export PLATFORMSH_ENVIRONMENT=${PLATFORMSH_ENVIRONMENT}" >> $BASH_ENV
