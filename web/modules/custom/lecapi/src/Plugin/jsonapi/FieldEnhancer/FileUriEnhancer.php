f8f382dc3d015c54d59330483a8b2d6884fb58e9
tan.nguyen <raucaixanh04@gmail.com>
2020-12-01 15:05:15 +0700

    phpcs fix

--- web/modules/custom/lecapi/src/Plugin/jsonapi/FieldEnhancer/FileUriEnhancer.php
+++ web/modules/custom/lecapi/src/Plugin/jsonapi/FieldEnhancer/FileUriEnhancer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\lecapi\Plugin\jsonapi\FieldEnhancer;
 
 use Drupal\jsonapi_extras\Plugin\ResourceFieldEnhancerBase;
@@ -15,7 +16,9 @@ use Shaper\Util\Context;
  */
 class FileUriEnhancer extends ResourceFieldEnhancerBase {
 
-
+  /**
+   * {@inheritdoc}
+   */
   protected function doTransform($data, Context $context) {
     if (isset($data['fully_qualified_url'])) {
       unset($data['fully_qualified_url']);
@@ -23,6 +26,9 @@ class FileUriEnhancer extends ResourceFieldEnhancerBase {
     return $data;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   protected function doUndoTransform($data, Context $context) {
     if ($data['value']) {
       $data['fully_qualified_url'] = file_create_url($data['value']);
@@ -38,4 +44,5 @@ class FileUriEnhancer extends ResourceFieldEnhancerBase {
       'type' => 'any',
     ];
   }
+
 }
