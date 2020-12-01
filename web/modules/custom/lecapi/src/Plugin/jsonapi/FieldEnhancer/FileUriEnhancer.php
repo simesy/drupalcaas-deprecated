9ca19145e96e059f6559c491c54cf9a130565622
tan.nguyen <raucaixanh04@gmail.com>
2020-12-01 15:03:39 +0700

    Add image style json api and add full url for uri field

---
+++ web/modules/custom/lecapi/src/Plugin/jsonapi/FieldEnhancer/FileUriEnhancer.php
@@ -0,0 +1,41 @@
+<?php
+namespace Drupal\lecapi\Plugin\jsonapi\FieldEnhancer;
+
+use Drupal\jsonapi_extras\Plugin\ResourceFieldEnhancerBase;
+use Shaper\Util\Context;
+
+/**
+ * Perform additional manipulations to uri fields.
+ *
+ * @ResourceFieldEnhancer(
+ *   id = "file_uri",
+ *   label = @Translation("Custom response file uri field."),
+ *   description = @Translation("Add fully_qualified_url to URI Field."),
+ * )
+ */
+class FileUriEnhancer extends ResourceFieldEnhancerBase {
+
+
+  protected function doTransform($data, Context $context) {
+    if (isset($data['fully_qualified_url'])) {
+      unset($data['fully_qualified_url']);
+    }
+    return $data;
+  }
+
+  protected function doUndoTransform($data, Context $context) {
+    if ($data['value']) {
+      $data['fully_qualified_url'] = file_create_url($data['value']);
+    }
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOutputJsonSchema() {
+    return [
+      'type' => 'any',
+    ];
+  }
+}
