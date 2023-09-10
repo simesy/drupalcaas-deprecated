c76b03be09aedc69f3c5e04fd65dece53ceb6c7f
Simon Hobbs <simon@hobbs.id.au>
2023-09-10 15:53:48 +1000

    Add lando dev. https://youtu.be/nVldMlh1AUg

--- web/sites/default/local.settings.php
+++ web/sites/default/local.settings.php
@@ -1,5 +1,17 @@
 <?php
 
+if (getenv('LANDO') === 'ON') {
+  $lando_info = json_decode(getenv('LANDO_INFO'), TRUE);
+  $databases['default']['default'] = [
+    'driver' => 'mysql',
+    'database' => $lando_info['database']['creds']['database'],
+    'username' => $lando_info['database']['creds']['user'],
+    'password' => $lando_info['database']['creds']['password'],
+    'host' => $lando_info['database']['internal_connection']['host'],
+    'port' => $lando_info['database']['internal_connection']['port'],
+  ];
+}
+
 $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/default/local.services.yml';
 
 $settings['twig_debug'] = TRUE;
